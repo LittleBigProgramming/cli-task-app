eacc8e141347efadbdb92d390072df9072659463
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-04-04 17:36:26 +0100

    Implementing command to complete tasks by using an arguement, taking the ID of the task.
    Small PSR-12 based change PSR-12 based changes.

--- src/CompleteCommand.php
+++ src/CompleteCommand.php
@@ -7,17 +7,16 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class AddCommand
+ * Class CompleteCommand
  * @package Acme
  */
-class AddCommand extends Command
+class CompleteCommand extends Command
 {
-
     public function configure()
     {
-        $this->setName('add')
-             ->setDescription('Add a new task')
-             ->addArgument('description', InputArgument::REQUIRED);
+        $this->setName('complete')
+            ->setDescription('Complete a task by its id')
+            ->addArgument('id', InputArgument::REQUIRED);
     }
 
     /**
@@ -27,14 +26,14 @@ class AddCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $description = $input->getArgument('description');
+        $id = $input->getArgument('id');
 
         $this->database->query(
-            'insert into tasks(description) values(:description)',
-            compact('description')
+            'delete from tasks where id = :id',
+            compact('id')
         );
 
-        $output->writeln('<info>Task Added!</info>');
+        $output->writeln('<info>Task Completed!</info>');
         $this->showTasks($output);
         return 1;
     }
